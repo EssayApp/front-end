0f37861d5d801e42adc999e7238d5e7a745ba426
mirage <mirage.kamran.pkt@gmail.com>
2021-03-28 00:46:51 -0700

    navbar styled

--- my-app/src/Components/Navigation/Navigation.js
+++ my-app/src/Components/Navigation/Navigation.js
@@ -1,27 +1,55 @@
 import React from "react";
-import './Navigation.css';
+import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/core/styles";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import PersonPinIcon from "@material-ui/icons/PersonPin";
+import LibraryBooksIcon from "@material-ui/icons/LibraryBooks";
+import HomeIcon from "@material-ui/icons/Home";
 import { Link } from "react-router-dom";
+import "./Navigation.css";
 
-const Navigation = () => {
+
+const useStyles = makeStyles({
+    root: {
+        flexGrow: 1,
+        // maxWidth: 500
+    }
+});
+
+function Navigation() {
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
     return (
-        <div>
-            <div className="Nav">
-                <h1>Essay App</h1>
-                <div>
-                    <Link to = "/">Home</Link>
+        <Paper square className={classes.root}>
+            <Tabs
+                value={value}
+                onChange={handleChange}
+                variant="fullWidth"
+                indicatorColor="secondary"
+                textColor="secondary"
+                aria-label="icon label tabs example"
+                centered
+            >
+                <div className="Nav">
+                    <Link to="/">
+                        <Tab icon={<HomeIcon />} label="HOME" />
+                    </Link>
+                    <Link to="/form">
+                        <Tab icon={<LibraryBooksIcon />} label="FORM" />
+                    </Link>
+                    <Link to="/sign-in">
+                        <Tab icon={<PersonPinIcon />} label="MEMBERS" />
+                    </Link>
                 </div>
-                <div>
-                    <Link to = "/form">Form</Link>
-                </div>
-                <div>
-                    <Link to = "/sign-in">Members</Link>
-                </div>
-                {/* <div>
-                    <Link to = "/sign-up">SignUp</Link>
-                </div> */}
-            </div>
-        </div>
+            </Tabs>
+        </Paper>
     );
-};
+}
 
 export default Navigation;
