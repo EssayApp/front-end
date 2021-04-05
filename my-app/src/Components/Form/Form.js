2169aacfe5e46f88894bb6f16eed50ba1e9b2ef9
mirage <mirage.kamran.pkt@gmail.com>
2021-04-04 18:04:24 -0700

    homepage designed

--- my-app/src/Components/Form/Form.js
+++ my-app/src/Components/Form/Form.js
@@ -3,14 +3,40 @@ import { render } from "react-dom";
 import "./Form.css";
 
 const Form = () => {
-    return(
-        <form>
-            <label>
-                Name:
-                <input type="text" name="name" />
-            </label>
-            <input type="submit" value="Submit" />
-        </form>
+    return (
+        <div class="ui_form">
+            <div class="three_fields">
+                <div class="field">
+                    <label style={{ marginRight: "30px"}}>Full Name</label>
+                    <input type="text" placeholder="Full Name"></input>
+                </div>
+                <div class="field">
+                    <label style={{ marginRight: "30px" }}>Class Name</label>
+                    <input type="text" placeholder="Class Name"></input>
+                </div>
+                <div class="field">
+                    <label style={{ marginRight: "30px" }}>
+                        Professor Name
+                    </label>
+                    <input type="text" placeholder="Professor Name"></input>
+                </div>
+                <div class="field">
+                    <label style={{ marginRight: "30px" }}>Date</label>
+                    <input type="text" placeholder=""></input>
+                </div>
+                <div class="field">
+                    <label style={{ marginRight: "30px" }}>
+                        Insert Essay text here:
+                    </label>
+                    <input type="text" placeholder=""></input>
+                </div>
+                <div>
+                    <div class="field">
+                        <button>Format</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };
 
